eb01f11490f06a99515d6a93728e5c3ee543a57c
waffleprinter <dwamsie@gmail.com>
2024-10-30 13:34:49 -0400

    added Tile class

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,24 @@
-int main() {
+#include <Tile.h>
+#include <SFML/Graphics.hpp>
 
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Test", sf::Style::Titlebar | sf::Style::Close);
+    sf::Event event{};
+
+    Tile tile = Tile(10, sf::Vector2f(200, 100));
+
+    while (window.isOpen()) {
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+
+        tile.draw(window);
+
+        window.display();
+    }
 }
