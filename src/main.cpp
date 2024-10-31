7cca5b971c81c2a2a1ac96815325f74b9d6815c0
waffleprinter <dwamsie@gmail.com>
2024-10-31 18:38:29 -0400

    reset

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,9 @@
-#include <Tile.h>
 #include <SFML/Graphics.hpp>
 
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Test", sf::Style::Titlebar | sf::Style::Close);
     sf::Event event{};
-
-    Tile tile = Tile(10, sf::Vector2f(200, 100));
 
     while (window.isOpen()) {
         while (window.pollEvent(event)) {
@@ -16,8 +13,6 @@ int main() {
         }
 
         window.clear();
-
-        tile.draw(window);
 
         window.display();
     }
